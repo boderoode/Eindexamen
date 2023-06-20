950c19bca34022aba291562fc6c1cdd4ef4d88ca
Boderoode <bo_deroode@outlook.com>
2023-06-20 16:39:31 +0200

    correct test added

---
+++ tests/Unit/unittesting.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class unittesting extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
